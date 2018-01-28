f7859f291e9b7574fed609182b4eb53883101a9c
Davixxa <w890ik660i@hotmail.com>
2018-01-28 16:38:56 +0100

    Stylesheet

--- src/davixxa_website/urls.py
+++ src/davixxa_website/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -24,3 +26,4 @@ urlpatterns = [
     url(r'^$', include("static_pages.urls",namespace='static')),
 
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
