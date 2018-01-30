e47cf8e2823ceaaf4175f46bc73ac33ba621b62c
Kasper Fredenslund <kasper.fredenslund@gmail.com>
2018-01-30 15:43:59 +0100

    type correction

--- src/static_pages/models.py
+++ src/static_pages/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Redirect(models.Model):
-    """A redirect class that 
+    """A redirect class
     """
     slug = models.SlugField(unique=True, primary_key=True)
     url = models.URLField(verbose_name="Redirect to URL")
