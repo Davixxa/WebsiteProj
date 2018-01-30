f66e10592e3bcfcdacea534e9e1c3734efb3bd03
Kasper Fredenslund <kasper.fredenslund@gmail.com>
2018-01-30 11:41:36 +0100

    Limit the number of posts to be displayed on frontpage (default=10)

--- src/static_pages/views.py
+++ src/static_pages/views.py
@@ -4,10 +4,14 @@ from posts.models import Post
 # Create your views here.
 
 def index(request):
-    query_set = Post.objects.all().filter(draft=0)
+    """Render the frontpage
+    """
+    # query_sets are lazy, so we can limit the number of
+    # posts by using list-slicing, here the first 10 posts
+    query_set = Post.objects.all().filter(draft=0)[:10]
 
     for query in query_set:
-         query.author = query.user.get_username()
+        query.author = query.user.get_username()
 
     context = {
 
